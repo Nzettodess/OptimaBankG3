ddc558f0f013b2abeafac96f89af44ac359496a6
Nzettodess <angkangheng@hotmail.com>
2025-09-18 22:20:51 +0800

    Fix naming and directory issue

--- google-login.php
+++ google-login.php
@@ -6,7 +6,7 @@ session_start();
 $client = new Google_Client();
 $client->setClientId("124073192361-hh1e2cqkt39h6octqa85a4gqeci3csp3.apps.googleusercontent.com");
 $client->setClientSecret("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");
-$client->setRedirectUri("http://localhost/Group3/google-callback.php");
+$client->setRedirectUri("http://localhost/OptimaBankG3/google-callback.php");
 $client->addScope("email");
 $client->addScope("profile");
 
